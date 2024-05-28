06f3618e27cb0a0b9f0018526a305813c96c96e6
Thomas Mahlberg <93132118+caol-ila@users.noreply.github.com>
2024-05-28 11:50:24 +0200

    Update KustoContinuousExportBulkLoader.cs

--- KustoSchemaTools/Parser/KustoLoader/KustoContinuousExportBulkLoader.cs
+++ KustoSchemaTools/Parser/KustoLoader/KustoContinuousExportBulkLoader.cs
@@ -4,7 +4,7 @@ namespace KustoSchemaTools.Parser.KustoLoader
 {
     public class KustoContinuousExportBulkLoader : KustoBulkEntityLoader<ContinuousExport>
     {
-        const string LoadContinuousExports = @".show database cslschema script 
+        const string LoadContinuousExports = @".show database hydro cslschema script 
 | parse-where  DatabaseSchemaScript with '.create-or-alter continuous-export ' EntityName:string ' to table ' ExternalTable:string ' with (forcedLatency=time(' ForcedLatency:timespan '), intervalBetweenRuns=time('IntervalBetweenRuns:timespan '), sizeLimit='SizeLimit:long', distributed='Distributed:bool', managedIdentity='ManagedIdentity:string') <| ' Query:string
 | project EntityName=trim("" "",EntityName), Body = bag_pack(
     'ExternalTable', ExternalTable,
