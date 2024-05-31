9727ffea21ec1c82107b17e03c7dcdaed0f944f0
Thomas Mahlberg <caol-ila@githubazure.com>
2024-05-31 10:21:14 +0200

    update docs

--- KustoSchemaTools/Model/Database.cs
+++ KustoSchemaTools/Model/Database.cs
@@ -33,14 +33,4 @@
 
     }
 
-    public class Deletions
-    {
-        public List<string> Tables { get; set; } = new List<string>();
-        public List<string> Columns { get; set; } = new List<string>();
-        public List<string> MaterializedViews { get; set; } = new List<string>();
-        public List<string> Functions { get; set; } = new List<string>();
-        public List<string> ContinuousExports { get; set; } = new List<string>();
-        public List<string> ExternalTables { get; set; } = new List<string>();
-    }
-
 }
---
+++ KustoSchemaTools/Model/Deletions.cs
@@ -0,0 +1,13 @@
+﻿namespace KustoSchemaTools.Model
+{
+    public class Deletions
+    {
+        public List<string> Tables { get; set; } = new List<string>();
+        public List<string> Columns { get; set; } = new List<string>();
+        public List<string> MaterializedViews { get; set; } = new List<string>();
+        public List<string> Functions { get; set; } = new List<string>();
+        public List<string> ContinuousExports { get; set; } = new List<string>();
+        public List<string> ExternalTables { get; set; } = new List<string>();
+    }
+
+}
