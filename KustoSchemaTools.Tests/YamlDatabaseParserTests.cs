1363126b8e839a954f739fb96d3122e1129f5e82
Oleksandr Slynko <alex-slynko@github.com>
2025-05-27 09:45:26 +0000

    Upgrade dotnet and packages

--- KustoSchemaTools.Tests/YamlDatabaseParserTests.cs
+++ KustoSchemaTools.Tests/YamlDatabaseParserTests.cs
@@ -14,7 +14,7 @@ namespace KustoSchemaTools.Tests.Parser
         [Fact]
         public async Task GetDatabase()
         {
-            var factory = new YamlDatabaseHandlerFactory()
+            var factory = new YamlDatabaseHandlerFactory<Model.Database>()
                 .WithPlugin(new TablePlugin())
                 .WithPlugin(new FunctionPlugin())
                 .WithPlugin(new MaterializedViewsPlugin())
@@ -25,9 +25,14 @@ namespace KustoSchemaTools.Tests.Parser
 
             Assert.NotNull(db);
             Assert.Equal(2, db.Tables.Count);
-            Assert.Equal(1, db.Functions.Count);
+            Assert.Single(db.Functions);
             Assert.Equal(6, db.Functions["UP"].Body.RowLength());
             Assert.Equal("DemoDatabase", db.Name);
+            var policies = db.Tables["sourceTable"].Policies;
+            Assert.NotNull(policies);
+            Assert.Equal("120d", policies.Retention);
+            Assert.Equal("120d", policies.HotCache);
+            Assert.Equal("Test team", db.Team);
             Assert.True(db.Tables["sourceTable"].RestrictedViewAccess);
             Assert.Equal("120d", db.Tables["tableWithUp"].RetentionAndCachePolicy.Retention);
             Assert.Equal("120d", db.Tables["sourceTable"].RetentionAndCachePolicy.HotCache);
