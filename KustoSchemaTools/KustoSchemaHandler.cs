40587a6a45ad69ed0b1569589e95a8c9faf9124d
Scott Seaton <sdseaton@github.com>
2025-06-26 11:25:46 -0400

    Uses raw databaseName for running queries

--- KustoSchemaTools/KustoSchemaHandler.cs
+++ KustoSchemaTools/KustoSchemaHandler.cs
@@ -36,23 +36,20 @@ namespace KustoSchemaTools
             var yamlHandler = YamlDatabaseHandlerFactory.Create(path, databaseName);
             var yamlDb = await yamlHandler.LoadAsync();
 
-            var escapedDbName = databaseName.BracketIfIdentifier();
-
-
             foreach (var cluster in clusters.Connections)
             {
-                Log.LogInformation($"Generating diff markdown for {Path.Combine(path, databaseName)} => {cluster}/{escapedDbName}");
+                Log.LogInformation($"Generating diff markdown for {Path.Combine(path, databaseName)} => {cluster}/{databaseName}");
 
-                var dbHandler = KustoDatabaseHandlerFactory.Create(cluster.Url, escapedDbName);
+                var dbHandler = KustoDatabaseHandlerFactory.Create(cluster.Url, databaseName);
                 var kustoDb = await dbHandler.LoadAsync();
-                var changes = DatabaseChanges.GenerateChanges(kustoDb, yamlDb, escapedDbName, Log);
+                var changes = DatabaseChanges.GenerateChanges(kustoDb, yamlDb, databaseName, Log);
 
                 var comments = changes.Select(itm => itm.Comment).Where(itm => itm != null).ToList();
 
         
                 isValid &= changes.All(itm => itm.Scripts.All(itm => itm.IsValid != false)) && comments.All(itm => itm.FailsRollout == false);
 
-                sb.AppendLine($"# {cluster.Name}/{escapedDbName} ({cluster.Url})");
+                sb.AppendLine($"# {cluster.Name}/{databaseName} ({cluster.Url})");
 
                 foreach (var comment in comments)
                 {
