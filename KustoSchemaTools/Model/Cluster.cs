865447ec2b268623a39bbb1a3ad1f8e7b88de917
Ashley Van Spankeren <25673124+ashleyvansp@users.noreply.github.com>
2025-07-04 19:15:56 +0000

    Add workload groups to the Cluster model

--- KustoSchemaTools/Model/Cluster.cs
+++ KustoSchemaTools/Model/Cluster.cs
@@ -6,6 +6,7 @@
         public string Url { get; set; }
         public List<DatabaseScript> Scripts { get; set; } = new List<DatabaseScript>();
         public ClusterCapacityPolicy? CapacityPolicy { get; set; }
+        public List<WorkloadGroup> WorkloadGroups { get; set; } = new List<WorkloadGroup>();
     }
 
 }
